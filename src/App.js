0c019da1cfa781fd5c7f175fad7dbba10561a77b
dejair8005 <68168713+dejair8005@users.noreply.github.com>
2023-06-07 16:57:05 -0300

    initial commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import { View } from 'react-native'
+
+import Primeiro from './components/Primeiro'
+
+
+export default () => (
+    <View>
+        <Primeiro />
+    </View>
+)
+
