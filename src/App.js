80db0ca90a41c43267422ab9b11b742e909b1471
dejair8005 <68168713+dejair8005@users.noreply.github.com>
2023-06-26 07:49:00 -0300

    Class Component

--- src/App.js
+++ src/App.js
@@ -1,7 +1,10 @@
 import React from 'react'
 import { SafeAreaView, StyleSheet } from 'react-native'
 
-import FlexboxV4 from './components/layout/FlexboxV4'
+
+
+import Mega from './components/mega/Mega'
+//import FlexboxV4 from './components/layout/FlexboxV4'
 //import FlexboxV3 from './components/layout/FlexboxV3'
 //import FlexboxV2 from './components/layout/FlexboxV1'
 //import FlexboxV1 from './components/layout/FlexboxV1'
@@ -27,9 +30,10 @@ import FlexboxV4 from './components/layout/FlexboxV4'
 
 export default () => (
     <SafeAreaView style={style.App}>
-        <FlexboxV4/>
+        <Mega qtdeNumeros={12}/>
 
         {/*
+        <FlexboxV4/>
         <FlexboxV3/>
         <FlexboxV2/>
         <FlexboxV1/>
