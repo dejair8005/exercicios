af31134fe30124c38f7d7a65ffd74e6f11d84d94
dejair8005 <68168713+dejair8005@users.noreply.github.com>
2023-07-03 15:32:28 -0300

    Random Nums

--- src/components/mega/Mega.js
+++ src/components/mega/Mega.js
@@ -1,32 +1,68 @@
 import React, { Component } from 'react'
-import { Text, TextInput } from 'react-native'
+import { Text, TextInput, Button, View } from 'react-native'
 import Estilo from '../estilo'
+
+import MegaNumero from './MegaNumeros'
 
 export default class Mega extends Component {
     state = {
-        qtdeNumeros: this.props.qtdeNumeros
+        qtdeNumeros: this.props.qtdeNumeros,
+        numeros: []  // Initialize the numeros state as an empty array
     }
 
-    alterarQtdeNumero(qtde) {
-        this.setState({ qtdeNumeros: qtde })
+    alterarQtdeNumero = (qtde) => {
+        this.setState({ qtdeNumeros: +qtde })
     }
+
+    gerarNumeroNaoContido = (nums) => {
+        const novo = parseInt(Math.random() * 60) + 1
+        return nums.includes(novo) ? this.gerarNumeroNaoContido(nums) : novo
+    }
+
+    gerarNumeros = () => {
+        const numeros = Array(this.state.qtdeNumeros)
+            .fill()
+            .reduce(n => [...n, this.gerarNumeroNaoContido(n)], [])
+            .sort((a, b) => a - b )
+        this.setState({ numeros })  // Set the state with the generated numbers
+    }
+
+    exibirNumeros = () => {
+        const nums = this.state.numeros
+        return nums.map(num => {
+            return <MegaNumero key={num} num={num} />
+        })
+    }
+
+    
 
     render() {
         return (
             <>
                 <Text style={Estilo.txtG}>
-                    Gerador de Mega-Sena 
-                {this.state.qtdeNumeros}
+                    Gerador de Mega-Sena {this.state.qtdeNumeros}
                 </Text>
-                <TextInput 
+                <TextInput
+                    keyboardType={'numeric'}
+                    style={{ borderBottomWidth: 1 }}
                     placeholder="Qtde de Números"
-                    value={this.state.qtdeNumeros}
+                    value={`${this.state.qtdeNumeros}`}
                     onChangeText={this.alterarQtdeNumero}
-                
                 />
+                <Button
+                    title='Gerar'
+                    onPress={this.gerarNumeros}
+                />
+                <View style={{
+                    marginTop: 20,
+                    flexDirection: 'row',
+                    flexWrap: 'wrap',
+                    justifyContent: 'center'
 
+                }}>
+                    {this.exibirNumeros()}
+                </View>
             </>
         )
     }
-
 }
